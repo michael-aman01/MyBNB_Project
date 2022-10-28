085592554bd7ae74eeb76df5454cd7c766c46cc8
michael <michael_aman92@icloud.com>
2022-10-28 13:52:45 -0700

    test

---
+++ frontend/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from './store'
+import csrfFetch, { restoreCSRF } from './store/csrf';
+import * as sessionActions from './store/session'
+
+
+const store = configureStore()
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+  window.csrfFetch = csrfFetch;
+  window.sessionActions = sessionActions;
+}
+
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+  
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+}
+
+
+
+if (
+  sessionStorage.getItem("currentUser") === null ||
+  sessionStorage.getItem("X-CSRF-Token") === null 
+) {
+  store.dispatch(sessionActions.restoreSession()).then(renderApplication);
+} else {
+  renderApplication();
+}
