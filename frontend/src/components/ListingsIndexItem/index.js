3dff87dda312ea5518ed2a6f0bddf35555cb2bd1
michael <michael_aman92@icloud.com>
2022-12-07 22:37:17 -0800

    listings edit

--- frontend/src/components/ListingsIndexItem/index.js
+++ frontend/src/components/ListingsIndexItem/index.js
@@ -1,16 +1,58 @@
 import "./listingsIndexItem.css"
 import reviewStar from "../../assets/Five_Pointed_Star_Solid.svg"
 
-import { useEffect} from "react";
+import { useEffect, useState} from "react";
 import {  useHistory } from "react-router-dom";
 
 
 export default function ListingsIndexItem({listing}){
     //add image fetch here
-   
+   const [currentPosition, setCurrentPosition] = useState()
+   const [distance, setDistance] = useState()
     const history = useHistory()
+    
 
+    function calcDistance(someListing){
+        
+        const lat = someListing.coordinates.position.lat
+        const lng = someListing.coordinates.position.lng
+        const radLat1 = Math.PI * lat /180
+        const radLat2 = Math.PI * currentPosition[0] /180
+        const radtheta = Math.PI * (currentPosition[0] - lat)/180
+        const dist1 = Math.sin(radLat1) * Math.sin(radLat2) + (Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radtheta))
+        const distance = (Math.acos(dist1) * 180/Math.PI) * 60 * 1.1515
+        return distance.toFixed(2)
+    }
+
+
+    function calcStats(reviewsArr){
+        const keys = Object.keys(reviewsArr[0])
+        const stats = {}
+        const unwanted = ["id","created_at","updated_at","text","user_id","listing_id"]
+        keys.filter(key => !unwanted.includes(key)).map(key => {
+            
+            let ratingsSum = reviewsArr.map(r => r[key]).reduce((a,b) => a + b, 0)
+            let stat = ratingsSum/reviewsArr.length
+            stats[key] = stat.toFixed(2)
+        })
+        const avg = Object.values(stats).map(val => parseInt(val)).reduce((a,b) => a +b, 0)/5
+        stats["avg"] = avg
+        console.log(stats)
+        return stats
+    }
+
+
+    
     useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                setCurrentPosition([position.coords.latitude,position.coords.longitude])
+            }
+          )
+          if(currentPosition !== undefined){
+            setDistance(calcDistance(listing))
+        }
+   
       
     },[])
     const handleClick = (e) => {
@@ -38,9 +80,9 @@ export default function ListingsIndexItem({listing}){
                            <p>{listing.city}, {listing.state}</p> 
                            <p id="distance-dates">
                   
-                              distance
+                              {distance} miles away
                             <br></br>
-                                dates
+                                -
                            </p>
           
                 
@@ -49,11 +91,18 @@ export default function ListingsIndexItem({listing}){
            
                     </div>
                     <div id="rating-box">
-                    <img alt="" id="review-star" src={reviewStar}></img>
-                        <b>     5.0
-                         
-                         
-                        </b>
+                        {
+                            listing.reviews.length > 0 ? 
+                            <>
+                            <img alt="" id="review-star" src={reviewStar}></img>
+                                {calcStats(listing.reviews).avg.toFixed(1)}
+                            <b>    
+                            </b>
+                            </>
+                            :
+                            null
+                        }
+              
                     </div>
                 </div>
 
