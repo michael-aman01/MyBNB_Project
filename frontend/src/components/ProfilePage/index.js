889e00748eef8d1b0aa2aa6a86200f2651a0bb1a
michael <michael_aman92@icloud.com>
2022-12-22 21:34:31 -0800

    edits

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -65,7 +65,7 @@ export default function ProfilePage(){
                        
                      </>
                      )}
-                       <div>fewf</div>
+
                 </div>
 
   
